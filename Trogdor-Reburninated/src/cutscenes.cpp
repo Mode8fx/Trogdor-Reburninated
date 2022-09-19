289545e4d8959ff1548e6bc3501a39c95bca76f8
Mips96 <57763469+Mips96@users.noreply.github.com>
2022-09-18 22:37:48 -0400

    More cutscene system additions

--- Trogdor-Reburninated/src/cutscenes.cpp
+++ Trogdor-Reburninated/src/cutscenes.cpp
@@ -18,25 +18,42 @@ void prepareCSO(CutsceneObject *csObj, SpriteObject *spriteObj) {
 	csObj->dstrect.h = csObj->sprite->frame_h;
 }
 
-void enterCSO(CutsceneObject *csObj, int x, int y, Sint8 frame, Sint8 form) {
+void enterCSO(CutsceneObject *csObj, int x, int y, Sint8 frame, Sint8 form, Sint8 frameTime, Sint8 vel_x, Sint8 vel_y) {
 	setCSOPos(csObj, x, y);
-	csObj->srcrect.x = spriteFrame(*csObj->sprite, frame);
-	csObj->srcrect.y = spriteForm(*csObj->sprite, form);
+	csObj->animFrame = frame;
+	updateCSOFrame(csObj);
+	csObj->animForm = form;
+	updateCSOForm(csObj);
+	csObj->animFrameTime = frameTime;
+	csObj->animFrameCounter = 0;
+	csObj->velocity_x = vel_x;
+	csObj->velocity_y = vel_y;
 	csObj->isVisible = true;
 }
 
-void renderCSO(CutsceneObject csObj) {
-	if (csObj.isVisible) {
-		renderSprite_game(*csObj.sprite, csObj.srcrect, csObj.dstrect);
+void renderCSO(CutsceneObject *csObj) {
+	if (csObj->isVisible) {
+		renderSprite_game(*csObj->sprite, csObj->srcrect, csObj->dstrect);
+		csObj->dstrect.x += csObj->velocity_x;
+		csObj->dstrect.y += csObj->velocity_y;
+		if (csObj->animFrameTime > 0) {
+			csObj->animFrameCounter++;
+			while (csObj->animFrameCounter >= csObj->animFrameTime) {
+				csObj->animFrameCounter -= csObj->animFrameTime;
+				PRINT((int)csObj->animFrame);
+				csObj->animFrame = (csObj->animFrame + 1) % csObj->sprite->numAnimFrames;
+				updateCSOFrame(csObj);
+			}
+		}
 	}
 }
 
-void setCSOFrame(CutsceneObject *csObj, Sint8 frame) {
-	csObj->srcrect.x = spriteFrame(*csObj->sprite, frame);
+void updateCSOFrame(CutsceneObject *csObj) {
+	csObj->srcrect.x = spriteFrame(*csObj->sprite, csObj->animFrame);
 }
 
-void setCSOForm(CutsceneObject *csObj, Sint8 form) {
-	csObj->srcrect.y = spriteForm(*csObj->sprite, form);
+void updateCSOForm(CutsceneObject *csObj) {
+	csObj->srcrect.y = spriteForm(*csObj->sprite, csObj->animForm);
 }
 
 void setCSOPos(CutsceneObject *csObj, int x, int y) {
@@ -49,10 +66,14 @@ void cutscene_level_4() {
 		case 420:
 			cutsceneIsPlaying = true;
 			loadAndPlaySound(SFX_CUTSCENE);
-			enterCSO(&cutscene_level_4_peasant, 21, 82, 1, 0);
+			enterCSO(&cutscene_level_4_peasant, 21, 82, 1, 0, 0, 0, 0);
+			enterCSO(&cutscene_level_4_trogdor, 246, 70, 0, 0, 0, -8, 0);
 			break;
 		case 448:
-			enterCSO(&cutscene_level_4_peasant, 24, 81, 0, 2);
+			enterCSO(&cutscene_level_4_peasant, 24, 81, 0, 2, 0, 0, 0);
+			break;
+		case 449:
+			enterCSO(&cutscene_level_4_trogdor, 25, 70, 0, 1, 0, 8, 0);
 			break;
 		case 492:
 			cutsceneIsPlaying = false;
@@ -61,5 +82,6 @@ void cutscene_level_4() {
 			break;
 	}
 	renderText(text_11_cutscene, font_serif_white_9);
-	renderCSO(cutscene_level_4_peasant);
+	renderCSO(&cutscene_level_4_peasant);
+	renderCSO(&cutscene_level_4_trogdor);
 }
--- Trogdor-Reburninated/src/cutscenes.h
+++ Trogdor-Reburninated/src/cutscenes.h
@@ -8,14 +8,20 @@ struct CutsceneObject {
 	SDL_Rect srcrect;
 	SDL_Rect dstrect;
 	bool isVisible = false;
+	Sint8 velocity_x;
+	Sint8 velocity_y;
+	Sint8 animFrame;
+	Sint8 animForm;
+	Sint8 animFrameTime;
+	Uint8 animFrameCounter;
 };
 
 extern void InitializeCutsceneObjects();
 extern void prepareCSO(CutsceneObject *, SpriteObject *);
-extern void enterCSO(CutsceneObject *, int, int, Sint8, Sint8);
-extern void renderCSO(CutsceneObject);
-extern void setCSOFrame(CutsceneObject *, Sint8);
-extern void setCSOForm(CutsceneObject *, Sint8);
+extern void enterCSO(CutsceneObject *, int, int, Sint8, Sint8, Sint8, Sint8, Sint8);
+extern void renderCSO(CutsceneObject *);
+extern void updateCSOFrame(CutsceneObject *);
+extern void updateCSOForm(CutsceneObject *);
 extern void setCSOPos(CutsceneObject *, int, int);
 extern void cutscene_level_4();
 
