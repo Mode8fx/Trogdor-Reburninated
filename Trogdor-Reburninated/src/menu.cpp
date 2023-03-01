bb0afb62ceef2ca3e9b09de4dbe86e28bc7f0f94
Mips96 <57763469+Mips96@users.noreply.github.com>
2023-02-28 21:27:01 -0500

    Font fix

--- Trogdor-Reburninated/src/menu.cpp
+++ Trogdor-Reburninated/src/menu.cpp
@@ -656,13 +656,12 @@ void InitializeMenus() {
 	// if the menu font would end up being the same as font_serif_white_6_mult, just use that instead of initializing an exclusive font
 	if (font_serif_white_6_mult.size == getDesiredFontSize(&font_serif_white_8)) {
 		menuFont = &font_serif_white_6_mult;
-		setFont(menuFont, "fonts/serif_v01.ttf", 8, 5, TTF_STYLE_NORMAL, color_white, true);
 	} else {
 		menuFont = &font_serif_white_8;
-		setFont(menuFont, "fonts/serif_v01.ttf", 8, 5, TTF_STYLE_NORMAL, color_white, true);
 	}
+	setFont(menuFont, "fonts/serif_v01.ttf", 8, 5, TTF_STYLE_NORMAL, color_white, true);
 	menuFont_frozen = &font_serif_gray_8;
-	setFont(&font_serif_gray_8, "fonts/serif_v01.ttf", 8, 5, TTF_STYLE_NORMAL, color_gray, false);
+	setFont(&font_serif_gray_8, "fonts/serif_v01.ttf", 8, 5, TTF_STYLE_NORMAL, color_gray, true);
 
 	/* Options Menu */
 	menu_main.prepareMenu(MENU_NUM_OPTIONS, 6, &sprite_menu_cursor, false, 1, 32 + (16 * (screenScale_menu >= 2)), 168 + (8 * (screenScale_menu >= 2)), 0, 25, 175, 25, 15, 0, 0, true);
