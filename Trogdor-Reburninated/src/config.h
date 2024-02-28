385490350f61367c01075a62a6a9a5c7e513e5f2
Mode8 <gateguy33@gmail.com>
2024-02-28 18:16:33 -0500

    Changed Gamecube root directory

--- Trogdor-Reburninated/src/config.h
+++ Trogdor-Reburninated/src/config.h
@@ -106,7 +106,7 @@ const string rootDir = "ux0:data/Trogdor-RB/";
 #elif defined(WII)
 const string rootDir = "sd:/apps/Trogdor-RB/";
 #elif defined(GAMECUBE)
-const string rootDir = "iso:/";
+const string rootDir = "sd:/Trogdor-RB/";
 #elif defined(THREEDS)
 const string rootDir = "sdmc:/3ds/Trogdor-RB/";
 #else
