0601fc3220bcf8a3904d04490b65c0bd067f5220
Mode8fx <gateguy33@gmail.com>
2024-08-11 18:19:10 -0400

    compile_all script overhaul
    - Added PC x64, PC x86, 3DS CIA, and Wii U WUHB. Now this script compiles every version except PSP
    - Compiled binaries are now moved to their respective distributable paths
    - Added more paths to variables
    - Split everything into functions
    - Silence WSL calls
    - Added a blank line after each version

--- Scripts/compile_all.bat
+++ Scripts/compile_all.bat
@@ -2,156 +2,241 @@
 
 :: Manual sleep amounts (because `start /wait` doesn't work...)
 set SLEEP_COMPILE=45
+set SLEEP_COMPILE_SHORT=5
 set SLEEP_CLEAN=2
 
 :: Path: repo (the folder that contains /src)
 set REPO=D:/GitHub/Trogdor-Reburninated/Trogdor-Reburninated
 set REPO_DKP=/d/GitHub/Trogdor-Reburninated/Trogdor-Reburninated
 set REPO_WSL=/mnt%REPO_DKP%
+set REPO_MSYS=%REPO_DKP%
+set OUTPUT_DIR=%REPO%/dist/new_version/Trogdor-Reburninated
+set RELEASE_RESOURCES=%REPO%/release-resources
 
 :: Path: devkitPro
 set DEVKITPRO=C:/devkitPro/msys2/mingw64.exe
 
-:: Path (MSYS2): Makefile directory
+:: Path: msys2
+set MSYS=C:/msys64/mingw64.exe
+
+:: Path: Makefile directory
 set MAKEFILES_DKP=%REPO_DKP%/Makefiles
+set MAKEFILES_MSYS=%MAKEFILES_DKP%
 set MAKEFILES_WSL=/mnt%MAKEFILES_DKP%
 
+:: Path: 3DS CIA Utilities
+set BANNERTOOL="D:/Utilities/3DS CIA Creation/bannertool"
+set MAKEROM="D:/Utilities/3DS CIA Creation/makerom-v0.18.3-win_x86_64/makerom.exe"
+set VERSION_3DS=545
 
+
+
+:: Makefile: Windows x64
+set MAKEFILE_MSYS_WINDOWS=%MAKEFILES_MSYS%/Makefile_pc
+set OUTPUT_WINDOWS=%OUTPUT_DIR%-x64/Trogdor-Reburninated.exe
+
+:: Makefile: Windows x86
+set MAKEFILE_MSYS_WINDOWS_X86=%MAKEFILES_MSYS%/Makefile_pc_x86
+set OUTPUT_WINDOWS_x86=%OUTPUT_DIR%-x86/Trogdor-Reburninated.exe
 
 :: Makefile: Linux
-set MAKEFILE_DKP_LINUX=%MAKEFILES_DKP%/Makefile_linux
 set MAKEFILE_WSL_LINUX=%MAKEFILES_WSL%/Makefile_linux
+set OUTPUT_LINUX=%OUTPUT_DIR%-linux/Trogdor-Reburninated
 
 :: Makefile: Gamecube
 set MAKEFILE_DKP_GC=%MAKEFILES_DKP%/Makefile_gc
-set MAKEFILE_WSL_GC=%MAKEFILES_WSL%/Makefile_gc
+set OUTPUT_GC=%OUTPUT_DIR%-gamecube/Trogdor-RB/boot.dol
 
 :: Makefile: Wii
 set MAKEFILE_DKP_WII=%MAKEFILES_DKP%/Makefile_wii
-set MAKEFILE_WSL_WII=%MAKEFILES_WSL%/Makefile_wii
+set OUTPUT_WII=%OUTPUT_DIR%-wii/apps/Trogdor-RB/boot.dol
 
 :: Makefile: Wii U
 set MAKEFILE_DKP_WII_U=%MAKEFILES_DKP%/make_wii_u.sh
-set MAKEFILE_WSL_WII_U=%MAKEFILES_WSL%/make_wii_u.sh
+set OUTPUT_WII_U=%OUTPUT_DIR%-wiiu/wiiu/apps/Trogdor-RB/Trogdor-Reburninated.rpx
+set OUTPUT_WII_U_WUHB=%OUTPUT_DIR%-wiiu/wiiu/apps/Trogdor-Reburninated.wuhb
 
 :: Makefile: Switch
 set MAKEFILE_DKP_SWITCH=%MAKEFILES_DKP%/Makefile_switch
-set MAKEFILE_WSL_SWITCH=%MAKEFILES_WSL%/Makefile_switch
+set OUTPUT_SWITCH=%OUTPUT_DIR%-switch/switch/Trogdor-RB/Trogdor-Reburninated.nro
 
 :: Makefile: 3DS
 set MAKEFILE_DKP_3DS=%MAKEFILES_DKP%/Makefile_3ds
-set MAKEFILE_WSL_3DS=%MAKEFILES_WSL%/Makefile_3ds
+set OUTPUT_3DS=%OUTPUT_DIR%-3ds/3ds/Trogdor-RB/Trogdor-Reburninated.3dsx
+set OUTPUT_3DS_CIA=%OUTPUT_DIR%-3ds-cia/cias/Trogdor-Reburninated.cia
 
 :: Makefile: PSP
-set MAKEFILE_DKP_PSP=%MAKEFILES_DKP%/Makefile_psp
 set MAKEFILE_WSL_PSP=%MAKEFILES_WSL%/Makefile_psp
+set OUTPUT_PSP=%OUTPUT_DIR%-psp/PSP/GAME/Trogdor-RB/EBOOT.PBP
 
 :: Makefile: Vita
-set MAKEFILE_DKP_VITA=%MAKEFILES_DKP%/make_vita.sh
 set MAKEFILE_WSL_VITA=%MAKEFILES_WSL%/make_vita.sh
+set OUTPUT_VITA=%OUTPUT_DIR%-vita/VPK/Trogdor-Reburninated.vpk
 
 :: Makefile: Xbox (original)
-set MAKEFILE_DKP_XBOX=%MAKEFILES_DKP%/Makefile_xbox
 set MAKEFILE_WSL_XBOX=%MAKEFILES_WSL%/Makefile_xbox
 
 :: Makefile: RG35XX
-set MAKEFILE_DKP_RG35XX=%MAKEFILES_DKP%/make_rg35xx.sh
 set MAKEFILE_WSL_RG35XX=%MAKEFILES_WSL%/make_rg35xx.sh
 
 
 
-:: Command: (WSL) Compile for Linux
-echo Linux: Compiling with WSL...
-wsl -e sh -c "cd %REPO_WSL% && make -f %MAKEFILE_WSL_LINUX%"
-echo Linux: Moving compiled binary to /dist/Trogdor-Reburninated-linux...
-mv %REPO%/trogdorrb %REPO%/dist/Trogdor-Reburninated-linux
-echo Linux: Cleaning up...
-wsl -e sh -c "cd %REPO_WSL% && make clean -f %MAKEFILE_WSL_LINUX%"
+:: Running compilation commands...
+call :compile_windows_x64
+rem call :compile_windows_x86
+rem call :compile_linux
+rem call :compile_gc
+rem call :compile_wii
+rem call :compile_wii_u
+rem call :compile_switch
+rem call :compile_3ds
+rem call :compile_vita
+rem call :compile_rg35xx
+rem call :compile_psp
 
-:: Command: (devkitPro) Compile for Gamecube
+echo Done.
+goto :eof
+
+
+
+:compile_windows_x64
+echo Windows x64: Compiling with MSYS2...
+start /wait "" %MSYS% /usr/bin/bash -lc "cd %REPO_MSYS%; make -f %MAKEFILE_MSYS_WINDOWS%"
+sleep %SLEEP_COMPILE%
+echo Windows x64: Moving compiled exe to %OUTPUT_WINDOWS%...
+mv %REPO%/Trogdor-Reburninated.exe %OUTPUT_WINDOWS%
+echo Windows x64: Cleaning up...
+start /wait "" %MSYS% /usr/bin/bash -lc "cd %REPO_MSYS%; make clean -f %MAKEFILE_MSYS_WINDOWS%"
+sleep %SLEEP_CLEAN%
+echo.
+goto :eof
+
+
+:compile_windows_x86
+echo Windows x86: Compiling with MSYS2...
+start /wait "" %MSYS% /usr/bin/bash -lc "cd %REPO_MSYS%; make -f %MAKEFILE_MSYS_WINDOWS_X86%"
+sleep %SLEEP_COMPILE%
+echo Windows x86: Moving compiled exe to %OUTPUT_WINDOWS_X86%...
+mv %REPO%/Trogdor-Reburninated.exe %OUTPUT_WINDOWS_X86%
+echo Windows x86: Cleaning up...
+start /wait "" %MSYS% /usr/bin/bash -lc "cd %REPO_MSYS%; make clean -f %MAKEFILE_MSYS_WINDOWS_X86%"
+sleep %SLEEP_CLEAN%
+echo.
+goto :eof
+
+:compile_linux
+echo Linux: Compiling with WSL...
+wsl -e sh -c "cd %REPO_WSL% && make -f %MAKEFILE_WSL_LINUX% > /dev/null 2>&1"
+echo Linux: Moving compiled binary to %OUTPUT_LINUX%...
+mv %REPO%/trogdorrb %OUTPUT_LINUX%
+echo Linux: Cleaning up...
+wsl -e sh -c "cd %REPO_WSL% && make clean -f %MAKEFILE_WSL_LINUX% > /dev/null 2>&1"
+echo.
+goto :eof
+
+:compile_gc
 echo Gamecube: Compiling with devkitPro...
 :: make is run twice to get around a makefile issue
 start /wait "" %DEVKITPRO% /usr/bin/bash -lc "cd %REPO_DKP%; make -f %MAKEFILE_DKP_GC%; make -f %MAKEFILE_DKP_GC%"
 sleep %SLEEP_COMPILE%
-echo Gamecube: Moving compiled dol to /dist/boot-gc.dol...
-mv %REPO%/boot.dol %REPO%/dist/boot-gc.dol
+echo Gamecube: Moving compiled dol to %OUTPUT_GC%...
+mv %REPO%/boot.dol %OUTPUT_GC%
 echo Gamecube: Cleaning up...
 start /wait "" %DEVKITPRO% /usr/bin/bash -lc "cd %REPO_DKP%; make clean -f %MAKEFILE_DKP_GC%"
 sleep %SLEEP_CLEAN%
+echo.
+goto :eof
 
-:: Command: (devkitPro) Compile for Wii
+:compile_wii
 echo Wii: Compiling with devkitPro...
 :: make is run twice to get around a makefile issue
 start /wait "" %DEVKITPRO% /usr/bin/bash -lc "cd %REPO_DKP%; make -f %MAKEFILE_DKP_WII%; make -f %MAKEFILE_DKP_WII%"
 sleep %SLEEP_COMPILE%
-echo Wii: Moving compiled dol to /dist/boot-wii.dol...
-mv %REPO%/boot.dol %REPO%/dist/boot-wii.dol
+echo Wii: Moving compiled dol to %OUTPUT_WII%...
+mv %REPO%/boot.dol %OUTPUT_WII%
 echo Wii: Cleaning up...
 start /wait "" %DEVKITPRO% /usr/bin/bash -lc "cd %REPO_DKP%; make clean -f %MAKEFILE_DKP_WII%"
 sleep %SLEEP_CLEAN%
+echo.
+goto :eof
 
-:: Command: (devkitPro) Compile for Wii U
-echo Wii U: Compiling with WSL...
+:compile_wii_u
+echo Wii U: Compiling with devkitPro...
 start /wait "" %DEVKITPRO% /usr/bin/bash -lc "cd %REPO_DKP%; sh %MAKEFILE_DKP_WII_U%"
 sleep %SLEEP_COMPILE%
-echo Wii U: Moving compiled rpx to /dist/Trogdor-Reburninated.rpx...
-mv %REPO%/build_wii_u/trogdorrb.rpx %REPO%/dist/Trogdor-Reburninated.rpx
+echo Wii U: Moving compiled rpx to %OUTPUT_WII_U%...
+mv %REPO%/build_wii_u/trogdorrb.rpx %OUTPUT_WII_U%
+echo Wii U: Creating WUHB in %OUTPUT_WII_U_WUHB%...
+start /wait "" %DEVKITPRO% /usr/bin/bash -lc "wuhbtool %OUTPUT_WII_U% %OUTPUT_WII_U_WUHB% --name=\"Trogdor: Reburninated\" --short-name=\"Trogdor: Reburninated\" --author=Mode8fx --icon=%RELEASE_RESOURCES%/logo_icon_vita.png --tv-image=%RELEASE_RESOURCES%/splash_screen_wiiu_tv.png --drc-image=%RELEASE_RESOURCES%/splash_screen_wiiu_gamepad.png"
+sleep %SLEEP_COMPILE_SHORT%
 echo Wii U: Cleaning up...
 rm -r %REPO%/build_wii_u
 sleep %SLEEP_CLEAN%
-echo Wii U: Didn't make WHUB... use WHUB Packager for that.
+echo.
+goto :eof
 
-:: Command: (devkitPro) Compile for Switch
+:compile_switch
 echo Switch: Compiling with devkitPro...
 start /wait "" %DEVKITPRO% /usr/bin/bash -lc "cd %REPO_DKP%; make -f %MAKEFILE_DKP_SWITCH%"
 sleep %SLEEP_COMPILE%
-echo Switch: Moving compiled nro to /dist/Trogdor-Reburninated.nro...
-mv %REPO%/Trogdor-Reburninated.nro %REPO%/dist/Trogdor-Reburninated.nro
+echo Switch: Moving compiled nro to %OUTPUT_SWITCH%...
+mv %REPO%/Trogdor-Reburninated.nro %OUTPUT_SWITCH%
 echo Switch: Cleaning up...
 start /wait "" %DEVKITPRO% /usr/bin/bash -lc "cd %REPO_DKP%; make clean -f %MAKEFILE_DKP_SWITCH%"
 sleep %SLEEP_CLEAN%
+echo.
+goto :eof
 
-:: Command: (devkitPro) Compile for 3DS
+:compile_3ds
 echo 3DS: Compiling with devkitPro...
 start /wait "" %DEVKITPRO% /usr/bin/bash -lc "cd %REPO_DKP%; make -f %MAKEFILE_DKP_3DS%"
 sleep %SLEEP_COMPILE%
-echo 3DS: Moving compiled 3dsx to /dist/Trogdor-Reburninated.3dsx...
-mv %REPO%/Trogdor-Reburninated.3dsx %REPO%/dist/Trogdor-Reburninated.3dsx
-echo 3DS: Moving compiled elf to /dist/Trogdor-Reburninated-3ds.elf...
-mv %REPO%/Trogdor-Reburninated.elf %REPO%/dist/Trogdor-Reburninated-3ds.elf
+echo 3DS: Moving compiled 3dsx to %OUTPUT_3DS%...
+mv %REPO%/Trogdor-Reburninated.3dsx %OUTPUT_3DS%
+echo 3DS: Creating CIA in %OUTPUT_3DS_CIA%...
+%MAKEROM% -f cia -o %OUTPUT_3DS_CIA% -elf %REPO%/Trogdor-Reburninated.elf -icon %RELEASE_RESOURCES%/icon_3ds.smdh -banner %RELEASE_RESOURCES%/banner_3ds.bnr -ver %VERSION_3DS% -rsf %RELEASE_RESOURCES%/app_3ds.rsf
 echo 3DS: Cleaning up...
 start /wait "" %DEVKITPRO% /usr/bin/bash -lc "cd %REPO_DKP%; make clean -f %MAKEFILE_DKP_3DS%"
 sleep %SLEEP_CLEAN%
-echo 3DS: Didn't make CIA... use makerom and bannertool for that.
+echo.
+goto :eof
 
-:: Command: (WSL) Compile for Vita
+:compile_vita
 echo Vita: Compiling with WSL...
-wsl -e sh -c "cd %REPO_WSL% && sh %MAKEFILE_WSL_VITA%"
-echo Vita: Moving compiled vpk to /dist/Trogdor-Reburninated.vpk...
-mv %REPO%/build_vita/trogdorrb.vpk %REPO%/dist/Trogdor-Reburninated.vpk
+wsl -e sh -c "cd %REPO_WSL% && sh %MAKEFILE_WSL_VITA% > /dev/null 2>&1"
+echo Vita: Moving compiled vpk to %OUTPUT_VITA%...
+mv %REPO%/build_vita/trogdorrb.vpk %OUTPUT_VITA%
 echo Vita: Cleaning up...
 rm -r %REPO%/build_vita
+echo.
+goto :eof
 
-:: Command: (WSL) Compile for RG35XX
+:compile_rg35xx
 echo RG35XX: Compiling with WSL...
-wsl -e sh -c "cd %REPO_WSL% && sh %MAKEFILE_WSL_RG35XX%"
-echo RG35XX: Moving compiled binary to /dist/trogdorrb-rg35xx...
-mv %REPO%/build_rg35xx/trogdorrb %REPO%/dist/trogdorrb-rg35xx
+wsl -e sh -c "cd %REPO_WSL% && sh %MAKEFILE_WSL_RG35XX% > /dev/null 2>&1"
+echo RG35XX: Moving compiled binary to %OUTPUT_RG35XX%...
+mv %REPO%/build_rg35xx/trogdorrb %OUTPUT_RG35XX%
 echo RG35XX: Cleaning up...
 rm -r %REPO%/build_rg35xx
+echo.
+goto :eof
 
-rem :: Command: (WSL) Compile for PSP (TODO: This doesn't work for some reason)
-rem echo PSP: Compiling with WSL...
-rem wsl -e sh -c "cd %REPO_WSL% && make -f %MAKEFILE_WSL_PSP%"
-rem echo PSP: Moving compiled binary to /dist/Trogdor-Reburninated-linux...
-rem mv %REPO%/trogdorrb %REPO%/dist/Trogdor-Reburninated-linux
-rem echo PSP: Cleaning up...
-rem wsl -e sh -c "cd %REPO_WSL% && make clean -f %MAKEFILE_WSL_PSP%"
+:compile_psp
+:: TODO: This doesn't work for some reason
+echo PSP: Compiling with WSL...
+wsl -e sh -c "cd %REPO_WSL% && make -f %MAKEFILE_WSL_PSP% > /dev/null 2>&1"
+echo PSP: Moving compiled binary to %OUTPUT_PSP%...
+mv %REPO%/trogdorrb %OUTPUT_PSP%
+echo PSP: Cleaning up...
+wsl -e sh -c "cd %REPO_WSL% && make clean -f %MAKEFILE_WSL_PSP% > /dev/null 2>&1"
+echo.
+goto :eof
 
-rem :: Command: (WSL) Compile for Xbox (original) (this doesn't work since ./activate breaks out of script)
+:compile_xbox
+:: (this doesn't work since ./activate breaks out of script)
 rem echo Xbox: Compiling with WSL...
-rem wsl -e sh -c "cd ~/nxdk/bin && ./activate && cd %REPO_WSL% && make -f %MAKEFILE_WSL_XBOX%"
-rem echo Xbox: Moving compiled binary to /dist/trogdorrb-xbox...
-rem mv %REPO%/build_xbox/trogdorrb %REPO%/dist/trogdorrb-xbox
-
-echo Done.
+rem wsl -e sh -c "cd ~/nxdk/bin && ./activate && cd %REPO_WSL% && make -f %MAKEFILE_WSL_XBOX% > /dev/null 2>&1"
+rem echo Xbox: Moving compiled binary to %OUTPUT_XBOX%...
+rem mv %REPO%/build_xbox/trogdorrb %OUTPUT_XBOX%
+rem echo.
+goto :eof
